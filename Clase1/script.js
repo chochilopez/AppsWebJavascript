9fbe83b87e7a62ceb2075a5d88913d1816e90e48
Gaston Lopez <lopezwgaston@gmail.com>
2024-05-03 17:47:03 -0300

    Clase numero 1
    Codigo visto en la primer clase. Hablamos de console.log, y hablamos de comentarios y buenas practicas.

---
+++ Clase1/script.js
@@ -0,0 +1,20 @@
+// Con dos contrabarras puedo escribir comentarios de una linea
+
+/*
+  Esto es un ejemplo de un comentario
+  de varias lineas
+*/
+
+// console.log('Hola desde un script importado a una pagina HTML');
+
+let numero = 15;
+// a mi variable le puse el valor 15
+
+
+// En esta sentencia estoy imprimiendo por consola el valor de la variable numero.
+console.log('El valor de la variable numero es: ', numero);
+
+/*
+  EN primer instancia cree una variable por que me pidio el profesor
+  y despues le di salido por la consola por que tambien me pidio el profesor
+*/
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AppsWebJavascript
 Introducción al desarrollo de aplicaciones web con JavaScript (ECMAScript 6)
+
+# ¡Bienvenidos a este curso!
+En este curso vas a aprender todo lo necesario para realizar aplicaciones web dinámicas a través de JavaScript. Asimismo aprenderás fundamentos de programación que te serán útiles para futuros aprendizajes.
+
+Si queres realizar una consulta enviá un email a lopezwgaston@gmail.com
