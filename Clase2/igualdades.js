3db6583d252cc8db02950d7048efd4700d12eedd
Gaston Lopez <lopezwgaston@gmail.com>
2024-05-10 13:04:06 -0300

    Clase2

---
+++ Clase2/igualdades.js
@@ -0,0 +1,11 @@
+// Igualdad estricta
+console.log(5 === 5);         // true
+console.log(5 === '5');       // false (tipos diferentes de datos)
+console.log(true === true);   // true
+console.log(true === 1);      // false (tipos diferentes de datos)
+
+// Igualdad abstracta
+console.log(5 == 5);          // true
+console.log(5 == '5');        // true (coercionado al mismo tipo de dato)
+console.log(true == true);    // true
+console.log(true == 1);       // true (coercionado al mismo tipo de dato)
