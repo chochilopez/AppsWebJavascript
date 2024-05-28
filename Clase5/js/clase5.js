da27b3c37398c27d7cf499c8249e0650f417b46e
Gaston Lopez <lopezwgaston@gmail.com>
2024-05-28 19:34:00 -0300

    Prep clase 5

--- Clase5/js/clase5.js
+++ Clase5/js/clase5.js
@@ -1,1 +1,20 @@
 'use strict'
+
+let llueve = window.confirm('Esta lloviendo?')
+if (llueve == true) {
+  // El codigo en bloque debajo de if solo se ejecutara si la expresion resulta verdadera
+  alert('Llevo paraguas');
+} else {
+  // El codigo en bloque debajo de else es opcional, se ejecutara si la expresion resulta false
+  alert('Lo dejo en casa');
+}
+
+let clima = 'Caluroso';
+
+if (clima = 'Frio') {
+	console.log('Me llevo campera');
+} else {
+ 	console.log('Salgo de remera');	
+}
+
+  
